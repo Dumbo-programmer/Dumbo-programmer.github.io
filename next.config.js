5c97cc569b244302c9b987a8d6f4fb568dad0642
Dumbo-programmer <rony.120285d2k@gmail.com>
2025-04-07 14:42:34 +0600

    next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+  }
+  
+  module.exports = nextConfig
+  
